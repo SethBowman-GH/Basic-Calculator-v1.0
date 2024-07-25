5373913fca0ea6740160d62762a0017cb037c844
SethBowman-GH <sbowman836@gmail.com>
2024-07-25 19:55:40 +0200

    Updated to v1.1
    Now added more functionality. Can use the pow() function and sqrt() function to do exponential and root calculations.

--- CalculatorCode .cpp
+++ CalculatorCode .cpp
@@ -1,39 +1,50 @@
 #include <iostream>
+#include <cmath> // For sqrt() and pow()
 using namespace std;
 
 int main() {
     char op;
     double num1, num2;
 
-    cout << "Enter operator (+, -, *, /): ";
+    cout << "Enter operator (+, -, *, /, ^, sqrt): ";
     cin >> op;
 
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
+    if (op == 'sqrt') {
+        cout << "Enter number: ";
+        cin >> num1;
+        cout << "sqrt(" << num1 << ") = " << sqrt(num1);
+    } else {
+        cout << "Enter two operands: ";
+        cin >> num1 >> num2;
 
-    switch(op) {
-        case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
-        break;
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
-        break;
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
-        break;
-        case '/':
-            if(num2 != 0)
-                cout << num1 << " / " << num2 << " = " << num1 / num2;
-            else
-                cout << "Error! Division by zero.";
-        break;
-        default:
-            cout << "Error! Operator is not correct.";
-        break;
+        switch(op) {
+            case '+':
+                cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+            case '-':
+                cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+            case '*':
+                cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+            case '/':
+                if(num2 != 0)
+                    cout << num1 << " / " << num2 << " = " << num1 / num2;
+                else
+                    cout << "Error! Division by zero.";
+            break;
+            case '^':
+                cout << num1 << " ^ " << num2 << " = " << pow(num1, num2);
+            break;
+            default:
+                cout << "Error! Operator is not correct.";
+            break;
+        }
     }
 
     return 0;
 }
+
 //
 // Created by sbowm on 7/25/2024.
 //
